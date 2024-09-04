c1a642b3fa10533206242be979dc7fae4e59fbf2
Eduardoesc3478 <eduardo.escobar3478@gmail.com>
2024-09-04 11:25:49 -0600

    Build(develop): se añadió proyecto a mi rama

---
+++ src/test/java/com/luislopez/Anonymous/AnonymousApplicationTests.java
@@ -0,0 +1,13 @@
+package com.luislopez.Anonymous;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnonymousApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
